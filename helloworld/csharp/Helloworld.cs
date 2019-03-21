2a6a4cd803c0bd61687ed389a3c04aa6cdecaef5
Kit Jonathan Te <kjonathante@gmail.com>
2019-03-20 20:43:18 -0700

    first commit

---
+++ helloworld/csharp/Helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace root
+{
+    class Helloworld
+    {
+        static void Main()
+        {
+            Console.Write("Hello ");
+            Console.WriteLine("World!");
+        }
+    }
+}
